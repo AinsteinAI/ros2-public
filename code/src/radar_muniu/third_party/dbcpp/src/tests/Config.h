4183bc2bef8cb2c736e5dc31817bf9ca969b5886
diaotianing <tianping@muniu.tech>
2026-06-10 10:19:25 +0800

    1. add dbcpp lib source code

---
+++ code/src/radar_muniu/third_party/dbcpp/src/tests/Config.h
@@ -0,0 +1,3 @@
+#define TEST_DBC "/home/ros2/work/ros2_ws/src/radar_muniu/third_party/dbcpp/src/tests/Test.dbc"
+constexpr const char* TEST_FILES_PATH = "/home/ros2/work/ros2_ws/src/radar_muniu/third_party/dbcpp/src/tests/test_files";
+#define TEST_DBC_WITH_SINGLE_COMMENTS "/home/ros2/work/ros2_ws/src/radar_muniu/third_party/dbcpp/src/tests/Test_single_comments.dbc"
